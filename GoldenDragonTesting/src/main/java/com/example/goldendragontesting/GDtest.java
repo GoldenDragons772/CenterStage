53100576f3eb100d81bc350bf1b58d8006c2ff27
OP <76495154+OPstriker@users.noreply.github.com>
2023-09-08 21:30:26 -0400

    Implement MeepMeep for Automation.
    - At stage for Implementing Autonomous code.

---
+++ GoldenDragonTesting/src/main/java/com/example/goldendragontesting/GDtest.java
@@ -0,0 +1,29 @@
+package com.example.goldendragontesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class GDtest {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, Math.toRadians(-135)))
+                                .splineTo(new Vector2d(-20, -20), Math.toRadians(0))
+                                .splineTo(new Vector2d(20, 20), Math.toRadians(0))
+                                .splineTo(new Vector2d(0, 0), Math.toRadians(-50))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_LIGHT)
+                .setDarkMode(false)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
