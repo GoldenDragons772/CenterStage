ba791a86669603fcd1730ab020c025633a9862c3
Gabriel Howe <me@gabe.how>
2023-10-23 16:29:26 -0400

    add pid test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboTag.java
@@ -1,23 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.RR.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.Vision.TagPipeline;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
 @Autonomous(name = "RobotTag", group = "Auto")
 public class RoboTag extends LinearOpMode {
+    int centerX = 160;
     private HuskyLens huskyLens;
+    private double lastError = 0;
+    private double lastTime = 0;
+
+    private int error(int blockXValue) {
+        return blockXValue - centerX;
+    }
 
     @Override
     public void runOpMode() {
@@ -38,13 +34,40 @@ public class RoboTag extends LinearOpMode {
         while (opModeIsActive()) {
             HuskyLens.Block[] blocks = huskyLens.blocks();
             telemetry.addData("Block count", blocks.length);
-            for (HuskyLens.Block i : blocks) {
-                telemetry.addData("Block", i.toString());
-            }
-
+            HuskyLens.Block block = blocks[0];
+            int error = error(block.x);
+            double pid = PID(error);
+            telemetry.addData("BlockData", block.toString());
+            telemetry.addData("PID", String.valueOf(pid));
             telemetry.update();
 
         }
+    }
+
+    private double PID(double error) {
+        double i = 0;
+        double time = System.currentTimeMillis();
+        final double kP = 0.5;
+        final double kI = 0.5;
+        final double kD = 0.5;
+        final double maxI = 1;
+
+        double p = kP * error;
+
+        i += kI * (error * (time - lastTime));
+
+        if (i > maxI) {
+            i = maxI;
+        }
+        if (i < -maxI) {
+            i = -maxI;
+        }
+
+        double d = kD * (error - lastError) / (time - lastTime);
+        lastError = error;
+        lastTime = time;
+
+        return d + i + p;
     }
 }
 
