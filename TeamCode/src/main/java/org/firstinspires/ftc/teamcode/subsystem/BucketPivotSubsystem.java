e8503ed8b5636c48e800ef7f69f903c981e8508f
OP <76495154+OPstriker@users.noreply.github.com>
2024-03-03 10:45:28 -0500

    Fixed issues with Bucket and Vison during competition.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/BucketPivotSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/BucketPivotSubsystem.java
@@ -8,10 +8,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Config
 public class BucketPivotSubsystem extends SubsystemBase {
 
-    public static double LeftBucketPivotDroppingPos = 0.76;
-    public static double LeftBucketPivotLoadingPos = 0.152;
-    public static double RightBucketPivotDroppingPos = 0.76;
-    public static double RightBucketPivotLoadingPos = 0.152;
+    public static double LeftBucketPivotDroppingPos = 0.61;
+    public static double LeftBucketPivotLoadingPos = 0.1;
+
+    public static double RightBucketPivotDroppingPos = 0.61;
+    public static double RightBucketPivotLoadingPos = 0.1;
 
 
     public enum BucketPivotPos {
