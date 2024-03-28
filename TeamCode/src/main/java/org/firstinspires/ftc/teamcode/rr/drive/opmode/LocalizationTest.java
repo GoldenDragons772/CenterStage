1b45bf766d3909413050de17b75f552433ac3835
OP <76495154+OPstriker@users.noreply.github.com>
2024-03-28 17:28:32 -0400

    Experimenting with LEDs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/opmode/LocalizationTest.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.rr.drive.opmode;
 
+import android.graphics.Color;
+
+import androidx.annotation.Px;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 import org.firstinspires.ftc.teamcode.rr.drive.MainMecanumDrive;
 
@@ -19,9 +26,13 @@ import org.firstinspires.ftc.teamcode.rr.drive.MainMecanumDrive;
 public class LocalizationTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        MainMecanumDrive drive = new MainMecanumDrive(hardwareMap);
+        //MainMecanumDrive drive = new MainMecanumDrive(hardwareMap);
 
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RevBlinkinLedDriver blinker = hardwareMap.get(RevBlinkinLedDriver.class, "blinker");
+
+        RevColorSensorV3 pxSense = hardwareMap.get(RevColorSensorV3.class, "bucketsense");
+//
+//        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
@@ -32,21 +43,53 @@ public class LocalizationTest extends LinearOpMode {
             double forward = Math.pow(gamepad1.right_stick_y, 2) * Math.signum(gamepad1.right_stick_y);
             double spin = Math.pow(gamepad1.left_stick_x, 2) * Math.signum(gamepad1.left_stick_x);
 
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -forward,
-                            -strafe,
-                            -spin
-                    )
-            );
+            if(gamepad1.dpad_up) {
+                blinker.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
+            }
+            if(gamepad1.dpad_down) {
+                blinker.setPattern((RevBlinkinLedDriver.BlinkinPattern.YELLOW));
+            }
 
-            drive.update();
+            NormalizedRGBA rgba = pxSense.getNormalizedColors();
 
-            Pose2d poseEstimate = drive.getPoseEstimate();
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
+            // Filter through colors
+
+            // Check if Red
+            if(pxSense.argb() > pxSense.blue() && pxSense.red() > pxSense.green()) {
+                blinker.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+            }
+
+            // Check if Green
+            else if(pxSense.green() > pxSense.red() && pxSense.green() > pxSense.blue()) {
+                blinker.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+            }
+
+            // Check if Blue
+            else if(pxSense.blue() > pxSense.red() && pxSense.blue() > pxSense.green()) {
+                blinker.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+            } else {
+                blinker.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_PARTY_PALETTE);
+            }
+
+            telemetry.addData("Red",  rgba.red);
+            telemetry.addData("Blue", rgba.blue);
+            telemetry.addData("Green", rgba.green);
             telemetry.update();
+//            drive.setWeightedDrivePower(
+//                    new Pose2d(
+//                            -forward,
+//                            -strafe,
+//                            -spin
+//                    )
+//            );
+//
+//            drive.update();
+//
+//            Pose2d poseEstimate = drive.getPoseEstimate();
+//            telemetry.addData("x", poseEstimate.getX());
+//            telemetry.addData("y", poseEstimate.getY());
+//            telemetry.addData("heading", poseEstimate.getHeading());
+//            telemetry.update();
         }
     }
 }
